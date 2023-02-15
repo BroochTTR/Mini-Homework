ac0618837032d044bc4d0ec126cbf4fd576b5124
BroochTTR <125259322+BroochTTR@users.noreply.github.com>
2023-02-16 03:59:19 +0700

    Add files via upload

---
+++ BT01-B-13.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main(){
+    double x,y;
+    cin>>x>>y;
+    double z=sqrt(x*x+y*y);
+    cout<<fixed<<setprecision(1)<<z;
+}
+
