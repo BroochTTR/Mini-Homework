80e08868f04fd3fb658d89dae19df3d2400daa8c
BroochTTR <125259322+BroochTTR@users.noreply.github.com>
2023-02-18 20:33:00 +0700

    Add files via upload

---
+++ BT01-B-16.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int year;
+    cin>>year;
+    if(year%4==0){
+        if(year%100==0){
+           if(year%400==0){
+            cout<<"true";
+           }else cout<<"false";
+
+        }else{
+            cout<<"true";
+        }
+
+    }else{
+        cout<<"false";
+    }
+}
+
