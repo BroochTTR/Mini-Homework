f4e9f1113e9fc994f0f367da9549c8e066466db6
BroochTTR <125259322+BroochTTR@users.noreply.github.com>
2023-02-18 20:18:36 +0700

    Update BT01-B-15.cpp

--- BT01-B-15.cpp
+++ BT01-B-15.cpp
@@ -8,6 +8,8 @@ int main(){
     string f0="a";
     string f1="b";
     string f2;
+    cout<<"f(0) la: a"<<endl;
+    cout<<"f(1) la: b"<<endl;
     for(int i=2;i<=10;i++){
         f2=f1+f0;
         cout<<"f("<<i<<")"<<" la: "<<f2<<endl;
